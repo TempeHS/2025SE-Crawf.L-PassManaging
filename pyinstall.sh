861afdd15859e2299f001cace5e60d961c925e38
Crawford L <110504159+DefNotCrawf@users.noreply.github.com>
2025-06-24 05:48:21 +0000

    Refactor directory handling in pyinstall.sh
    - Changed: Updated the condition to check for the existence of the ./dist/final/ directory instead of ./dist/.
    - Removed: The previous check and removal of the entire ./dist/ directory.

--- pyinstall.sh
+++ pyinstall.sh
@@ -10,14 +10,14 @@ git stash && git pull
 pip install -r requirements.txt --upgrade
 
 # Remove the ./dist directory and all contents
-if [ -d "./dist/" ]; then
+if [ -d "./dist/final/" ]; then
 	echo "Removing existing ./dist directory..."
-	rm -rf ./dist/
+	rm -rf ./dist/final/
 else
-	echo "No existing ./dist directory found."
+	echo "No existing ./dist/final/ directory found."
 fi
 
-# Build executables; .exe files go in ./dist
+# Build executables using PyInstaller
 pyinstaller --clean --noconfirm --debug all --noconsole --specpath ./build encode.py
 
 pyinstaller --clean --noconfirm --debug all --noconsole --specpath ./build decode.py
