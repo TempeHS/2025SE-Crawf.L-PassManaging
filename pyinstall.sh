07e6682aae8a1077efdac5fc97207e8e537b0231
Crawford L <110504159+DefNotCrawf@users.noreply.github.com>
2025-06-24 10:15:35 +0000

    Refactor file merging process and update script references
    - Added: hash_file_mt.py for merging files and SHA2-256 hash checks
    - Changed: Updated path reference in pyinstall.sh to point to the new location of hash_file_mt.py
    - Removed: Previous implementation of file merging

--- pyinstall.sh
+++ pyinstall.sh
@@ -23,4 +23,4 @@ pyinstaller --clean --noconfirm --debug all --noconsole --specpath ./build encod
 pyinstaller --clean --noconfirm --debug all --noconsole --specpath ./build decode.py
 
 # Merge files using cross-platform Python script
-python hash_file_mt.py
+python ./building/hash_file_mt.py
